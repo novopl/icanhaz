7b7160d4aa26afb8fd173c49f699b04f022d7073
Mateusz 'novo' Klos <novopl@gmail.com>
2015-02-16 16:45:07 +0000

    Some appengine helpers

---
+++ plugins/djangoappengine.sh
@@ -0,0 +1,32 @@
+declare -A COMMANDZ
+COMMANDZ=(
+    [devsrv]='cmd_gae_devsrv'
+    [deploy]='cmd_gae_deploy'
+)
+export COMMANDZ
+
+#----------------------------------------------------------------------------//
+function cmd_gae_devsrv() {
+    icanhaz clean
+    sysmsg "STARTIN TEH SERVR"
+    ./manage.py runserver $@
+}
+#----------------------------------------------------------------------------//
+function cmd_gae_deploy() {
+    sysmsg "UPLOADIN TEH APP 2 TEH SERVR"
+    local lxmlpath=${PYLIBDIR}/lxml
+    $(test -d ${lxmlpath})
+    local haslxml=$?
+
+    if [  ${haslxml} -eq 0 ]; then
+        sysmsg "Moving lxml away for deployment"
+        mv ${PYLIBDIR}/lxml ../
+    fi
+
+    ./manage.py deploy $@
+
+    if [  ${haslxml} -eq 0 ]; then
+        sysmsg "Moving lxml back into libs"
+        mv ../lxml ${PYLIBDIR}
+    fi
+}
